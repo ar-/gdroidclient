e007ec8a42de62a8e2388d93c58950ae8e7b0428
Andreas Redmer <ar-gitlab@abga.be>
2018-11-08 19:22:02 +0800

    populated main toolbar with 'myapps' only, removed all unsused items in there

--- app/src/main/java/org/gdroid/gdroid/MainActivity.java
+++ app/src/main/java/org/gdroid/gdroid/MainActivity.java
@@ -411,22 +411,14 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-            //Intent myIntent = new Intent(this, TempTabActivity.class);
-            //myIntent.putExtra("key", value); //Optional parameters
-            //this.startActivity(myIntent);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        if (id == R.id.nav_my_apps) {
+            navigation.setSelectedItemId(getItemIdForHomeScreenMenuItem("myapps"));
+//        } else if (id == R.id.nav_gallery) {
+//        } else if (id == R.id.nav_slideshow) {
+//        } else if (id == R.id.nav_manage) {
+//        } else if (id == R.id.nav_share) {
+//        } else if (id == R.id.nav_send) {
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
