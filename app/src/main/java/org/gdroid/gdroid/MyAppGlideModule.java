4df2a748b4e29416ca7b27d57c9eb28206460481
Andreas Redmer <ar-gdroid@abga.be>
2019-01-08 21:57:39 +0800

    updated glide and put 60 seconds timeut in place. fixes #69 works great if the repo is overloaded because loading a single image can easily exceed 2.5 seconds. right now I got more like 12 seconds

--- app/src/main/java/org/gdroid/gdroid/MyAppGlideModule.java
+++ app/src/main/java/org/gdroid/gdroid/MyAppGlideModule.java
@@ -18,8 +18,36 @@
 
 package org.gdroid.gdroid;
 
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
 
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
 @GlideModule
-public final class MyAppGlideModule extends AppGlideModule {}
+public final class MyAppGlideModule extends AppGlideModule {
+
+
+    @Override
+    public void registerComponents(Context context, Glide glide, Registry registry) {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+        builder
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS);
+
+        OkHttpClient okHttpClient = builder.build();
+
+        registry.replace(GlideUrl.class, InputStream.class,
+                new OkHttpUrlLoader.Factory(okHttpClient));
+    }
+
+}
