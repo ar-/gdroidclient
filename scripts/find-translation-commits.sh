bf7d740c351cd7c6a25f6b49558a630292f3e575
Andreas Redmer <ar-gdroid@abga.be>
2019-01-10 18:03:29 +0800

    added 2 maint scripts

---
+++ scripts/find-translation-commits.sh
@@ -0,0 +1,4 @@
+#git log | grep -v "Andreas Redmer"| grep -B1 -A3 Author
+#git log app/src/main/res/| grep -v "Andreas Redmer"| grep -B1 -A3 Author
+git log app/src/main/res/ | grep -B4 "Translated using Weblate"
+
